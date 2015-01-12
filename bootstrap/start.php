82ed0f44ffcbb8851de1efc23bbcf34bd92c698a
apache <apache@silver-VirtualBox.(none)>
2015-01-12 21:59:39 +0200

    ...

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('vitya-HP-655-Notebook-PC'),
+	'local' => array('silver-VirtualBox'),
 
 ));
 
