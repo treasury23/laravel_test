dd5b9b4b6e69e1487c81e39144b1d8d1244f7354
apache <apache@silver-VirtualBox.(none)>
2015-01-12 17:44:55 +0200

    add div to form button

--- app/views/signup.blade.php
+++ app/views/signup.blade.php
@@ -10,7 +10,7 @@
         {{ Form::text('username', null,  array('class' => 'form-control', 'placeholder' => 'Логин')) }}
         {{ Form::text('email', null, array('class' => 'form-control', 'placeholder' => 'E-mail')) }}
         {{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'Пароль')) }}
-        {{ Form::submit('Войти', array('class' => 'btn btn-lg btn-primary btn-block')) }}
+        <div style="padding:20px">{{ Form::submit('Отправить', array('class' => 'btn btn-lg btn-primary btn-block')) }}</div>
 
     {{ Form::close() }}
 </div>
