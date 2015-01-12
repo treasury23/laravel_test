a5f77815a7c887efbbcaec186e703eefc5f8bb78
Viktor Musienko <treasury23@gmail.com>
2015-01-12 11:00:15 +0200

    init

---
+++ app/routes.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+
+//Route::get('users', function () {
+//    return View::make('users');
+//});
+
+Route::get('users', function () {
+    $users = User::all();
+
+    return View::make('users')->with('users', $users);
+});
+
+Route::get('users1', 'HomeController@showWelcome');
+
+
+
+Route::get('user/{name?}', function ($name = null) {
+    return $name;
+});
